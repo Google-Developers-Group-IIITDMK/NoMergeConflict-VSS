b0d10e9dd241d5951898fae58350ea2e6123c2a9
ReddyVikranth <vikranth2028@gmail.com>
2025-09-21 10:47:16 +0530

    Adding graph component to app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Graph from "./components/graph_visualizing/Graph";
 
 function App() {
+  const myValues = [5, 10, 3, 8, 15, 20, 12, 18];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ width: "80%", margin: "auto", marginTop: "50px" }}>
+      <h2>My Graph</h2>
+      <Graph values={myValues} />
     </div>
   );
 }
