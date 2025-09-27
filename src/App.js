ed8016d0351fe0852d7051fc1bef8df9a98bf215
ReddyVikranth <vikranth2028@gmail.com>
2025-09-27 18:41:48 +0530

    built and integrated loginpage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TradingDesk from './components/TradingDesk';
 import './index.css';
+import Login from "./components/login"
 
 function App() {
+  const [username, setUsername] = useState("");
+
+  const handleLogin = (user) => {
+    setUsername(user);
+  };
+
+  if(!username) return <Login onLogin={handleLogin} />;
+  
   return (
     <div className="app">
-      <TradingDesk />
+      <TradingDesk username={username} />
     </div>
   );
 }
